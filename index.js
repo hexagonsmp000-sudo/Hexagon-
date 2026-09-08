afcb40944113684918d00a2b24b41e0467a8f4fc
hexagonsmp000-sudo <hexagonsmp000@gmail.com>
2026-09-08 06:24:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
+
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Bot is ONLINE'));
-app.listen(process.env.PORT || 10000);
+app.get('/', (req,res)=> res.send('Bot is ONLINE'));
+app.listen(process.env.PORT || 10000, ()=>console.log('Web server on 10000'));
 
 const bedrock = require('bedrock-protocol');
 function start() {
-  const client = bedrock.createClient({
-    host: 'play.hexagonsmp.fun',
-    port: 25571,
-    username: 'HexagonBot',
-    offline: true,
-    version: '1.21.70'
-  });
-  client.on('spawn', () => console.log('BOT SPAWNED SUCCESS!'));
-  client.on('close', () => setTimeout(start, 5000));
-  client.on('error', console.log);
+ console.log('Trying to connect to play.hexagonsmp.fun:25571...');
+ const client = bedrock.createClient({
+   host: 'play.hexagonsmp.fun',
+   port: 25571,
+   username: 'HexagonBot',
+   offline: true
+ });
+ client.on('spawn', () => console.log('BOT SPAWNED SUCCESS! Bot is in server'));
+ client.on('text', (p) => console.log(`<${p.source_name}> ${p.message}`));
+ client.on('close', (e) => { console.log('Disconnected:', e); setTimeout(start, 5000); });
+ client.on('error', (e) => console.log('ERROR:', e.message));
 }
 start();
